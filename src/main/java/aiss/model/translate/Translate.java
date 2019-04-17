72eb09e26746323b47c20e20882b72d54e9284ad
Rafa <2erfernandez@gmail.com>
2019-04-17 20:46:48 +0200

    microsoft translate Java class

---
+++ src/main/java/aiss/model/translate/Translate.java
@@ -0,0 +1,45 @@
+
+package aiss.model.translate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "translations"
+})
+public class Translate {
+
+    @JsonProperty("translations")
+    private List<Translation> translations = null;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("translations")
+    public List<Translation> getTranslations() {
+        return translations;
+    }
+
+    @JsonProperty("translations")
+    public void setTranslations(List<Translation> translations) {
+        this.translations = translations;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
